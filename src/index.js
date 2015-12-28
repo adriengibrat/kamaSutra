82d2133a814595ab0075475214563bf450538992
Adrien Gibrat <a.gibrat@oodrive.fr>
2015-12-28 15:36:53 +0100

    simpler signature, expose overflowContainer method

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ let spaceRegEx = /\s+/
 
 let overflowContainer = (element) => closest(closest(element, isRelative) || element.parentElement, isOverflowBox)
 
-export default (classNames, constraint = containsBox) => {
-	let removeClassNamesRegexp = new RegExp(
-		`\\s*\\b(?:${arrayUnique(classNames.join(' ').split(spaceRegEx)).join('|')})\\b`,
-		'g'
-	)
+export default function kamaSutra (...classNames) {
+	let uniqueClassnames = arrayUnique(classNames.join(' ').split(spaceRegEx))
+	let removeClassNamesRegexp = new RegExp(`\\s*\\b(?:${uniqueClassnames.join('|')})\\b`, 'g')
 
 	return (element, container = overflowContainer(element)) => {
 		if (!element || element.nodeType !== Node.ELEMENT_NODE || !element.parentElement) {
@@ -18,13 +16,16 @@ export default (classNames, constraint = containsBox) => {
 		let containerBox = container.getBoundingClientRect()
 		let originalClassName = element.className || ''
 		let cleanClassName = originalClassName.replace(removeClassNamesRegexp, '')
-		classNames
+		let optimalPosition = classNames
 			.some((className) => {
-				element.className = cleanClassName && `${cleanClassName} ${className}` || className
-				//console.log(coverBox(containerBox, element.getBoundingClientRect()))
-				return constraint(containerBox, element.getBoundingClientRect())
-			}) || (element.className = originalClassName)
+				element.className = cleanClassName ? `${cleanClassName} ${className}` : className
+				return containsBox(containerBox, element.getBoundingClientRect())
+			})
 
-		return element
+		if (!optimalPosition) {
+			element.className = originalClassName
+		}
 	}
 }
+
+kamaSutra.overflowContainer = overflowContainer
